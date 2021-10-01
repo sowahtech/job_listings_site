094012dbdc57c768ba71497e23d65c9daf879b45
justice sowah <justice.sowah18@gmail.com>
2021-10-01 20:58:15 +0000

    passed each object of data to Job component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Job from './Job';
 
 const Jobs = ({data}) => {
 	console.log(data);
     return (
-        <div>
-          Jobs  
+        <div className = "jobs">
+          {data.map(d => {
+			  return <Job data={d}/>
+		  })} 
         </div>
-    )
-}
+    );
+};
 
-export default Jobs
+export default Jobs;
