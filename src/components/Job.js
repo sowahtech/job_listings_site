9945e1a9b60b94cf066f70736b98d699005790ad
justice sowah <justice.sowah18@gmail.com>
2021-10-01 22:09:40 +0000

    deconstructed data from props.data in Job component

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 const Job = (props) => {
+
+    const {
+        company,
+        contract,
+        featured,
+        id,
+        languages,
+        level,
+        location,
+        logo,
+        position,
+        postedAt,
+        role,
+        tools
+    } = props.data;
     return (
         <div className="job-container">
             {props.data.company}
