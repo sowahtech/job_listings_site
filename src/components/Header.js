b9f52b7aebfc0c97d0f4ad55900fee95a4c969d0
justice sowah <justice.sowah18@gmail.com>
2021-10-04 18:54:37 +0000

    deconstructed deleteKeywords method && passed it to the remove button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import close from './images/icon-remove.svg';
 
-const Header = ({keywords}) => {
+const Header = ({keywords, removeKeywords}) => {
     return (
        <div className="header-container">
            {
                keywords.map((key,id) => {
-                   return <button key={id}>{key}</button>
+                   return <li key={id}>{key}
+                   <button onClick = {() => removeKeywords(key)}>
+                        <img src={close} alt="" />
+                   </button>
+                   </li>
                })
            }
        </div>
