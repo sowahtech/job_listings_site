6737e815bf2d165b980009bf254a7287aa4a203e
justice sowah <justice.sowah18@gmail.com>
2021-10-04 21:26:52 +0000

    destructured clearAll method and passed it as a callback function to clear link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const Header = ({keywords, removeKeywords}) => {
+const Header = ({keywords, removeKeywords, clearAll}) => {
     return (
        <div className="header-container">
-           {
-               keywords.map((key,id) => {
-                   return <li key={id}>{key}
-                        <button onClick={() => removeKeywords(key)}>
-                            <img src= {require('./images/icon-remove.svg')} alt="" />
-                        </button>
-                   </li>
-               })
-           }
+            <ul>
+                {
+                    keywords.map((key,id) => {
+                        return (
+                            <li key={id}>
+                                {key}
+                                <button onClick={() => removeKeywords(key)}>
+                                    <img src= {require('./images/icon-remove.svg')} alt="" />
+                                </button>
+                            </li>
+                        )
+                    })
+                }
+                <a href="#" onClick={() => clearAll()}>Clear</a>
+            </ul>
        </div>
     )
 }
