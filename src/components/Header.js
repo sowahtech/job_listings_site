1c6928f27f8e3ed1f8fd815c11adea6c884f98fa
justice sowah <justice.sowah18@gmail.com>
2021-10-04 19:31:37 +0000

    method for clearing all keywords added to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,11 @@ const Header = ({keywords, removeKeywords}) => {
        <div className="header-container">
            {
                keywords.map((key,id) => {
-                   return <button key={id}>{key}</button>
+                   return <li key={id}>{key}
+                        <button onClick={() => removeKeywords(key)}>
+                            <img src= {require('./images/icon-remove.svg')} alt="" />
+                        </button>
+                   </li>
                })
            }
        </div>
