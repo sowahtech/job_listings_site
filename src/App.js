81f6eebf1d256ff83a68f94468e40b171d9d40d1
justice sowah <justice.sowah18@gmail.com>
2021-10-04 17:27:41 +0000

    Header component is rendered when there is data in filterKeywords state

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
       <div className="header"></div>
-	  	
+	  	{filterKeywords.length > 0 && <Header keywords = {filterKeywords} />}
       <Jobs data={data}/>
     </div>
   );
